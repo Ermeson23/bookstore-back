7399f9012719b201e757a2f2d70c1859ee280793
Ermeson23 <joseenem35@gmail.com>
2023-08-11 13:15:44 -0300

    creating delete to category

--- src/main/java/com/ermeson/bookstore/controller/CategoryController.java
+++ src/main/java/com/ermeson/bookstore/controller/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> update(@PathVariable Integer id, @RequestBody CategoryDTO categoryDTO) {
         Category newCategory = categoryService.update(id, categoryDTO);
         return ResponseEntity.ok().body(new CategoryDTO(newCategory));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/ermeson/bookstore/service/CategoryService.java
+++ src/main/java/com/ermeson/bookstore/service/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ermeson.bookstore.dto.CategoryDTO;
@@ -37,6 +38,11 @@ public class CategoryService {
         category.setName(categoryDTO.getName());
         category.setDescription(categoryDTO.getDescription());
         return categoryRepository.save(category);
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        categoryRepository.deleteById(id);
     }
     
 }
