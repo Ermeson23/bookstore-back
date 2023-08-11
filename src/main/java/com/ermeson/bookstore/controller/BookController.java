f66665cafacb1b7cc61b590d555dc2fc88e17691
Ermeson23 <joseenem35@gmail.com>
2023-08-11 17:24:48 -0300

    listing all books by category

--- src/main/java/com/ermeson/bookstore/controller/BookController.java
+++ src/main/java/com/ermeson/bookstore/controller/BookController.java
@@ -1,12 +1,17 @@
 package com.ermeson.bookstore.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ermeson.bookstore.dto.BookDTO;
 import com.ermeson.bookstore.model.Book;
 import com.ermeson.bookstore.service.BookService;
 
@@ -23,6 +28,12 @@ public class BookController {
         return ResponseEntity.ok().body(book);        
     }
 
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> findAll(@RequestParam(value = "category", defaultValue = "0") Integer id_cat) {
+        List<Book> list = bookService.findAll(id_cat);
+        List<BookDTO> listDTO = list.stream().map(obj -> new BookDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
 
+    }
 
 }
--- src/main/java/com/ermeson/bookstore/service/BookService.java
+++ src/main/java/com/ermeson/bookstore/service/BookService.java
@@ -1,5 +1,6 @@
 package com.ermeson.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,19 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private CategoryService categoryService;
+
     public Book findById(Integer id) {
         Optional<Book> book = bookRepository.findById(id);
         return book.orElseThrow(() -> new ObjectNotFoundException(
             "Livro não encontrado! Id: " + id + ", tipo: " + Book.class.getName()
         ));
+    }
+
+    public List<Book> findAll(Integer id_cat) {
+        categoryService.findById(id_cat);
+        return bookRepository.findAllByCategory(id_cat);
     }
 
 }
