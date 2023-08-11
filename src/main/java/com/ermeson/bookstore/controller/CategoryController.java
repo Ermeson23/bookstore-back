259254dc4427cdbce4b788253e51923896e9cb61
Ermeson23 <joseenem35@gmail.com>
2023-08-10 23:40:12 -0300

    Creating findAll to category

--- src/main/java/com/ermeson/bookstore/controller/CategoryController.java
+++ src/main/java/com/ermeson/bookstore/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package com.ermeson.bookstore.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ermeson.bookstore.dto.CategoryDTO;
 import com.ermeson.bookstore.model.Category;
 import com.ermeson.bookstore.service.CategoryService;
 
@@ -16,6 +20,13 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<Category> list = categoryService.findAll();
+        List<CategoryDTO> listDTO = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
     
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Integer id) {
